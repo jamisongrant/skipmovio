90642316b8590f68813ae6c1463e22a1c92ec9be
jamisongrant <jgbatchelor@gmail.com>
2017-03-30 13:46:19 -0600

    add render template for pages show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :movies
   get "/pages/:page" => "pages#show"
+  root "pages#show", page: "home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
