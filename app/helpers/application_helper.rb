8635f731c47aaf5254847835cd15f77108058066
jamisongrant <jgbatchelor@gmail.com>
2017-03-30 15:22:43 -0600

    add navbar classes and image file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,36 @@ module ApplicationHelper
   # def copyright_generator
   #   BatchelorViewTool::Renderer.copyright 'Jamie Batchelor', "All rights reserved"
   # end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        # url: about_path,
+        # title: 'About'
+      },
+      {
+        # url: contact_path,
+        # title: 'Contact'
+      },
+      {
+        url: movies_path,
+        title: 'Movies'
+      },
+
+    ]
+  end
+
+  # def nav_helper style, tag_type
+  #   nav_links = ''
+
+  #   nav_items.each do |item|
+  #     nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+  #   end
+
+  #   nav_links.html_safe
+  # end
 end
